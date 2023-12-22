8ced933202481b638186f67b9fbc6be625cac0a9
Blockyyy <88585273+Blockyyy@users.noreply.github.com>
2023-12-22 11:56:09 +0100

    Replace PSS

---
+++ levels/pss/area_3/macro.inc.c
@@ -0,0 +1,16 @@
+const MacroObject pss_area_3_macro_objs[] = {
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_ring_horizontal, 0, 259, 3009, -2013, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_ring_horizontal, 0, 2562, 3477, -482, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_ring_horizontal, 0, 2360, 4432, 1413, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 0, 135, 5466, 1017, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 0, 22, 6014, -1412, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_coin_line_horizontal, 0, 210, 7409, -1307, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_goomba, 0, 1785, 4015, 583, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_goomba, 0, 1169, 4897, 1213, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_goomba, 0, 1435, 3098, -1156, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_piranha_plant, 0, 108, 5539, -477, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_piranha_plant, 90, -948, 7074, -2345, 0x00000000),
+	MACRO_OBJECT_WITH_BEH_PARAM(macro_floor_switch_hidden_objects, 0, 129, 5185, 1964, 0x00000000),
+	MACRO_OBJECT_END(),
+};
+
---
+++ levels/pss/texscroll.inc.c
@@ -0,0 +1,90 @@
+void scroll_pss_dl_main_002_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 100;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(pss_dl_main_002_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(-0.5 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_pss_dl_island_002_mesh_layer_1_vtx_0() {
+	int i = 0;
+	int count = 20;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(pss_dl_island_002_mesh_layer_1_vtx_0);
+
+	deltaY = (int)(0.75 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_pss_dl_main_003_mesh_layer_1_vtx_2() {
+	int i = 0;
+	int count = 322;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(pss_dl_main_003_mesh_layer_1_vtx_2);
+
+	deltaY = (int)(0.75 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_pss_dl_main_001_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 103;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(pss_dl_main_001_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(-0.5 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
+void scroll_pss() {
+	scroll_pss_dl_main_002_mesh_layer_5_vtx_0();
+	scroll_pss_dl_island_002_mesh_layer_1_vtx_0();
+	scroll_pss_dl_main_003_mesh_layer_1_vtx_2();
+	scroll_pss_dl_main_001_mesh_layer_5_vtx_0();
+};
